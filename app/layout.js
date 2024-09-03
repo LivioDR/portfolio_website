ee248d60cbb30f315d5562d3b09883b6c177acdf
LivioDR <liviodavidreinoso@yahoo.com.ar>
2024-09-03 17:27:28 -0400

    set global styles set GoogleFont wrapper

--- app/layout.js
+++ app/layout.js
@@ -1,7 +1,7 @@
-import { Inter } from "next/font/google";
+import { Merriweather } from "next/font/google";
 import "./globals.css";
 
-const inter = Inter({ subsets: ["latin"] });
+const merry = Merriweather({ weight: '400', subsets: ["latin"] });
 
 export const metadata = {
   title: "Create Next App",
@@ -11,7 +11,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={merry.className}>{children}</body>
     </html>
   );
 }
