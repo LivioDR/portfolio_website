d0ec03f2e0c0ba0bc8b9d6bfc3f572a6f560e114
LivioDR <liviodavidreinoso@yahoo.com.ar>
2024-09-05 16:16:30 -0400

    added ProjectScreen to home added ProgressBar on app open set global backgroundColor

--- app/page.js
+++ app/page.js
@@ -1,7 +1,9 @@
 'use client'
 import { useState, useEffect } from "react";
+import { ProgressBar } from "react-loader-spinner";
 import Header from "./components/Header/Header";
 import HomeScreen from "./components/HomeScreen/HomeScreen";
+import ProjectsScreen from "./components/ProjectsScreen/ProjectsScreen";
 import './globals.css'
 
 export default function Home() {
@@ -24,10 +26,35 @@ export default function Home() {
     }
   },[])
 
-  return (
-    <>
-      <Header format={viewport?.format}/>
-      <HomeScreen viewport={viewport}/>
-    </>
-  );
+
+  if(viewport?.format){
+    return (
+      <>
+        <Header format={viewport?.format}/>
+        <HomeScreen viewport={viewport}/>
+        <ProjectsScreen/>
+      </>
+    );
+  }
+  else
+  {
+    return(
+      <div style={{
+        width: '100%',
+        height: '100vh',
+        backgroundColor: 'var(--light-main)',
+        display: 'grid',
+        placeItems: 'center',
+      }}>
+      <ProgressBar
+        barColor='var(--main-color)'
+        borderColor='var(--main-color)'
+        visible={true}
+        ariaLabel="progress-bar-loading"
+        width={200}
+        height={200}
+      />
+      </div>
+    )
+  }
 }
