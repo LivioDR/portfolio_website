3d9bd3380c83af2666923e07edb70b69567a2682
LivioDR <liviodavidreinoso@yahoo.com.ar>
2024-09-05 18:31:45 -0400

    added SpeedInsights

--- app/page.js
+++ app/page.js
@@ -1,6 +1,7 @@
 'use client'
 import { useState, useEffect } from "react";
 import { ProgressBar } from "react-loader-spinner";
+import { SpeedInsights } from "@vercel/speed-insights/next"
 import Header from "./components/Header/Header";
 import HomeScreen from "./components/HomeScreen/HomeScreen";
 import ProjectsScreen from "./components/ProjectsScreen/ProjectsScreen";
@@ -30,6 +31,7 @@ export default function Home() {
   if(viewport?.format){
     return (
       <>
+        <SpeedInsights/>
         <Header format={viewport?.format}/>
         <HomeScreen viewport={viewport}/>
         <ProjectsScreen/>
