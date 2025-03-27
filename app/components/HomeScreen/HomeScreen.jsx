32fe1728ff946b08c2b542c0e387a56fcfcfe330
LivioDR <liviodavidreinoso@yahoo.com.ar>
2025-03-27 18:55:04 -0400

    refactor HomeScreen to use an array for body text and add Spacer component for improved layout

--- app/components/HomeScreen/HomeScreen.jsx
+++ app/components/HomeScreen/HomeScreen.jsx
@@ -5,10 +5,12 @@ import Title from "./Title";
 import BodyText from "./BodyText";
 import CTAButton from "./CTAButton";
 import BodyImage from "./BodyImage";
+import { Spacer } from "../Common/Spacer";
 
 const EXPERIENCE_YEARS = Number(new Date().getFullYear()) - 2020
 
-const bodyText = `I'm a Front-End Developer and AI Software Engineer based in London, Ontario 🇨🇦 specializing in scalable web applications, AI chatbot development, and SaaS platforms. With over ${EXPERIENCE_YEARS} years of experience, I build cutting-edge, AI-enhanced digital solutions that streamline business operations and enhance user experiences.`
+const bodyTexts = [`I'm a Front-End Developer and AI Software Engineer based in London, Ontario 🇨🇦 specializing in scalable web applications, AI chatbot development, and SaaS platforms. With over ${EXPERIENCE_YEARS} years of experience, I build cutting-edge, AI-enhanced digital solutions that streamline business operations and enhance user experiences.`]
+
 const styles = {
     identifier: {
         height: '150px',
@@ -52,7 +54,9 @@ const HomeScreen = ({viewport}) => {
                 isPortrait &&
                 <div style={{...styles.column, width: '100%', paddingBottom: '20%', paddingTop: '20%', gap: '50px'}}>
                     <Title/>
-                    <BodyText text={bodyText}/>
+                    { bodyTexts.map((text, index) => 
+                        <BodyText key={index} text={text}/>
+                    )}
                     <CTAButton styleToUse={viewport.format} hrefValue={'#contact'}/>
                 </div>
             }
@@ -61,7 +65,9 @@ const HomeScreen = ({viewport}) => {
                 <>
                     <div style={styles.column}>
                         <Title/>
-                        <BodyText text={bodyText}/>
+                        { bodyTexts.map((text, index) => 
+                            <BodyText key={index} text={text}/>
+                        )}
                         <CTAButton styleToUse={viewport.format} hrefValue={'#contact'}/>
                     </div>
                     <div style={{...styles.column, ...styles.column2}}>
@@ -70,6 +76,7 @@ const HomeScreen = ({viewport}) => {
                 </>
             }
         </div>
+        <Spacer flipped={true} darkens={true}/>
         </>
     )
 }
