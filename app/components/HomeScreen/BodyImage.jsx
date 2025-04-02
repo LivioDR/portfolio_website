8b36053d97d953da7d5674b6bf7a1b5cb5062564
LivioDR <liviodavidreinoso@yahoo.com.ar>
2025-04-02 11:32:16 -0400

    refactor BodyImage component for improved styling and layout consistency

--- app/components/HomeScreen/BodyImage.jsx
+++ app/components/HomeScreen/BodyImage.jsx
@@ -2,15 +2,16 @@ import React from "react";
 import Image from "next/image";
 
 const styles = {
-    borderRadius: 50,
     boxShadow: 'rgba(0, 0, 0, 0.25) 0px 54px 55px, rgba(0, 0, 0, 0.12) 0px -12px 30px, rgba(0, 0, 0, 0.12) 0px 4px 6px, rgba(0, 0, 0, 0.17) 0px 12px 13px, rgba(0, 0, 0, 0.09) 0px -3px 5px',
-    margin: '10% auto 20% auto',
 }
 
-const BodyImage = ({viewportSize = {width: 20}}) => {
-    const imgWidth = viewportSize.width * 0.25
+const BodyImage = () => {
+
+    const imgWidth = 500
+
     return(
         <Image
+            className="w-10/12 mx-auto mt-[10%] mb-[20%] rounded-4xl"
             src={'/LivioPhoto.JPG'}
             alt={'photo of Livio Reinoso'}
             width={imgWidth}
