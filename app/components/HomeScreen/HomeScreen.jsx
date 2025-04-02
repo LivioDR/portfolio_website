72e49c18f9c92fc81a2576c3011f511295b04e22
LivioDR <liviodavidreinoso@yahoo.com.ar>
2025-04-02 11:20:38 -0400

    refactor HomeScreen component for improved layout and responsive design

--- app/components/HomeScreen/HomeScreen.jsx
+++ app/components/HomeScreen/HomeScreen.jsx
@@ -1,82 +1,59 @@
-'use client'
-import React, { useEffect } from "react";
-import { useState } from "react";
 import Title from "./Title";
 import BodyText from "./BodyText";
 import CTAButton from "./CTAButton";
 import BodyImage from "./BodyImage";
 import { Spacer } from "../Common/Spacer";
+import { cn } from "@/lib/utils";
 
 const EXPERIENCE_YEARS = Number(new Date().getFullYear()) - 2020
 
 const bodyTexts = [`I'm a Front-End Developer and AI Software Engineer based in London, Ontario 🇨🇦 specializing in scalable web applications, AI chatbot development, and SaaS platforms. With over ${EXPERIENCE_YEARS} years of experience, I build cutting-edge, AI-enhanced digital solutions that streamline business operations and enhance user experiences.`]
 
 const styles = {
-    identifier: {
-        height: '150px',
-        backgroundColor: 'var(--light-main)',
-        width: '100%',
-    },
-    container: {
-        display: 'flex',
-        flexDirection: 'row',
-        width: '100%',
-        height: '100%',
-        minHeigth: '80vh',
-        backgroundColor: 'var(--light-main)',
-    },
     column: {
-        display: 'flex',
         flexDirection: 'column',
-        width: '60%',
         justifyContent: 'center',
         alignItems: 'center',
-    },
-    column2: {
-        width: '40%'
     }
 }
 
 const HomeScreen = ({viewport}) => {
 
-    const [isPortrait, setIsPortrait] = useState(viewport.format === 'portrait' ? true : false)
-
-    useEffect(()=>{
-        setIsPortrait(viewport.format === 'portrait' ? true : false)
-    },[viewport])
-
     return(
         <>
-        <div style={styles.identifier} id="about-me">
+        <div className="min-h-[150px] w-full bg-light" id="about-me">
         </div>
-        <div style={styles.container}>
-            {
-                isPortrait &&
-                <div style={{...styles.column, width: '100%', paddingBottom: '20%', paddingTop: '20%', gap: '50px'}}>
-                    <Title/>
-                    { bodyTexts.map((text, index) => 
-                        <BodyText key={index} text={text}/>
-                    )}
-                    <CTAButton styleToUse={viewport.format} hrefValue={'#contact'}/>
-                </div>
-            }
-            {
-                !isPortrait &&
-                <>
-                    <div style={styles.column}>
-                        <Title/>
-                        { bodyTexts.map((text, index) => 
-                            <BodyText key={index} text={text}/>
-                        )}
-                        <CTAButton styleToUse={viewport.format} hrefValue={'#contact'}/>
-                    </div>
-                    <div style={{...styles.column, ...styles.column2}}>
-                        <BodyImage viewportSize={viewport}/>
-                    </div>
-                </>
-            }
+        <div className="relative flex flex-row w-full d min-h-[80vh] bg-light">
+
+            {/* MOBILE DISPLAY */}
+            <div 
+            className={cn("md:hidden flex")}
+            style={{...styles.column, width: '100%', paddingBottom: '20%', paddingTop: '20%', gap: '50px'}}
+            >
+                <Title/>
+                { bodyTexts.map((text, index) => 
+                    <BodyText key={index} text={text}/>
+                )}
+                <CTAButton styleToUse={viewport.format} hrefValue={'#contact'}/>
+            </div>
+
+            {/* DESKTOP DISPLAY */}
+            <div 
+            className={cn("md:flex hidden w-3/5")}
+            style={styles.column}
+            >
+                <Title/>
+                { bodyTexts.map((text, index) => 
+                    <BodyText key={index} text={text}/>
+                )}
+                <CTAButton styleToUse={viewport.format} hrefValue={'#contact'}/>
+            </div>
+            <div style={{...styles.column}} className="md:flex hidden w-2/5">
+                <BodyImage viewportSize={viewport}/>
+            </div>
+            
+            <Spacer flipped={false} darkens={true}/>
         </div>
-        <Spacer flipped={false} darkens={true}/>
         </>
     )
 }
